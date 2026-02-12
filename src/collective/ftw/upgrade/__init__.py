ded5e8937ef7848ed7f0405e8c7d694db4b8a3d2
Mathias Leimgruber <m.leimgruber@webcloud7.ch>
2026-02-12 08:13:09 -0500

    Back to development: 4.0.0a4

--- src/collective/ftw/upgrade/__init__.py
+++ src/collective/ftw/upgrade/__init__.py
@@ -1,7 +1,7 @@
 # pylint: disable=W0104
 # W0104: Statement seems to have no effect
 
-__version__ = "4.0.0a3"
+__version__ = "4.0.0a4.dev0"
 
 
 from collective.ftw.upgrade.progresslogger import ProgressLogger
