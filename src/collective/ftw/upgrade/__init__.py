f4f5941b9a128dfbe7e152fd08db6235af0fa22a
ale-rt <alessandro.pisa@gmail.com>
2026-03-26 10:50:12 +0100

    Add module alias for ftw.upgrade.interfaces
    Fixes #26

--- src/collective/ftw/upgrade/__init__.py
+++ src/collective/ftw/upgrade/__init__.py
@@ -8,6 +8,19 @@ from collective.ftw.upgrade.progresslogger import ProgressLogger
 from collective.ftw.upgrade.step import UpgradeStep
 
 
+try:
+    import ftw.upgrade.interfaces
+
+    del ftw.upgrade.interfaces
+except ImportError:
+    from .bbb import interfaces
+    from plone.app.upgrade.utils import alias_module
+
+    alias_module("ftw.upgrade.interfaces", interfaces)
+    del interfaces
+    del alias_module
+
+
 UpgradeStep
 
 ProgressLogger
